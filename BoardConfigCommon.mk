847a64f761f9ee4c16a6db00e88d75eaa6e8f677
SkyMinus <minus_rav@qq.com>
2025-02-02 09:59:59 +0800

    sm8550-common: Drop screen density config
    Change-Id: Ibad75d207f09cf5760677f6ff45947c37f00d61f
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,9 +58,6 @@ $(call soong_config_set, android_hardware_audio, run_64bit, true)
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
-# Display
-TARGET_SCREEN_DENSITY := 540
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
