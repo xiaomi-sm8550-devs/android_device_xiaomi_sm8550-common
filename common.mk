0f1d69218e0977382e1fd2790a9782a0fd39dd11
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 02:04:23 +0200

    sm8550-common: Enable CameraX extensions support
    - Set `ro.camerax.extensions.enabled=true` in both system.prop and odm.prop
    - Add `camerax-vendor-extensions.jar` to system_ext
    - Grant required permission via `camerax-vendor-extensions.xml`
    
    Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.
    
    Change-Id: Ifb163a67308116a363d4fa9240f7a0a1ca0038ef

--- common.mk
+++ common.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera Extensions permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
