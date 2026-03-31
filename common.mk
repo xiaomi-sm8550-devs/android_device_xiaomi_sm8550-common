88628138cce41551a2db7a0b2e3ff4f0f5835a8e
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2026-03-31 22:06:47 +0800

    sm8550-common: Fixup Lineage Health charge control
    The `night_charging` node is giving us very inconsistent results when it comes to charge limiting.
    So we are going to switch back to `input_suspend`.
    
    Change-Id: I30dfd107bdcd4e86057f0559a32c30d6a7a3cfa5
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -228,7 +228,7 @@ PRODUCT_COPY_FILES += \
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/night_charging)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 PRODUCT_PACKAGES += \
